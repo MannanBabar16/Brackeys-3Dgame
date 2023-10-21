2a8e41e8a149461193986ee4985066d250a01bce
MannanBabar16 <mannan.babar16@gmail.com>
2023-10-21 16:44:02 +0500

    Player Movement + Camera Followw

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    [SerializeField]
+    private float forwardSpeed = 200f;
+
+    [SerializeField]
+    private float sideWaysSpeed = 500f;
+
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +22,20 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(0f, 0f, forwardSpeed * Time.deltaTime);
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddForce(sideWaysSpeed * Time.deltaTime, 0f, 0f);
+        }
+
+        if(Input.GetKey("a")) 
+        {
+            rb.AddForce((sideWaysSpeed * -1) * Time.deltaTime, 0f, 0f);
+        }
     }
 }
